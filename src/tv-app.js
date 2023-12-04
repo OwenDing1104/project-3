c85afa068c0bdd081ba1a520f193d54900be85ea
OwenDing1104 <karsaowen@gmail.com>
2023-12-03 19:04:33 -0500

    Test8

--- src/tv-app.js
+++ src/tv-app.js
@@ -1,154 +1,142 @@
-// tv-app.js
 import { LitElement, html, css } from 'lit';
+import "@lrnwebcomponents/video-player/video-player.js";
+import "./tv-channel.js"; 
 
 export class TvApp extends LitElement {
-  constructor() {
-    super();
-    this.name = 'OwenDingTV';
-    this.source = new URL('../assets/channels.json', import.meta.url).href;
-    this.courses = [];
-    this.activeItem = null;
-  }
-
-  static get tag() {
-    return 'tv-app';
-  }
-
   static get properties() {
     return {
       name: { type: String },
       source: { type: String },
-      courses: { type: Array },
+      listings: { type: Array },
       activeItem: { type: Object },
+      currentIndex: { type: Number },
     };
   }
 
   static get styles() {
     return css`
-    :host {
-      display: flex;
-      margin: 0;
-      padding: 0;
-      font-family: Arial, sans-serif;
-      height: 100vh; 
-    }
-    
-    .sidebar {
-      width: 250px; 
-      overflow-y: auto;
-      padding: 20px; 
-      box-sizing: border-box;
-      border-right: 1px solid #ccc;
-    }
-    
-    .main-content {
-      flex-grow: 1;
-      padding: 20px;
-      box-sizing: border-box;
-      overflow-y: auto;
-    }
-    
-    h2, h3 {
-      margin: 0 0 20px 0;
-    }
-    
-    p {
-      margin: 0 0 10px 0;
-      cursor: pointer;
-      word-break: break-word;
-    }
-    
-    p:hover {
-      text-decoration: underline;
-    }
-    
-    video {
-      width: 700px;
-      max-height: 500px;
-    }
+      :host {
+        display: block;
+        padding: 16px;
+        height: 100vh;
+      }
+      .lecture-container {
+        display: grid;
+        grid-template-columns: 300px 1fr;
+        gap: 32px;
+        height: calc(100% - 32px);
+      }
+      .lecture-screen {
+        grid-column: 2;
+        grid-row: 1;
+        position: relative;
+      }
+      .tv-channel-list {
+        grid-column: 1;
+        grid-row: 1;
+        overflow-y: auto;
+      }
+      video-player {
+        width: 700px;
+        max-height: 500px;
+      }
+      #navigationButtons {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 20px;
+      }
+      button {
+        padding: 8px 16px;
+        background: #007bff;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        cursor: pointer;
+        font-size: 16px;
+      }
+      button:disabled {
+        background: #cccccc;
+      }
     `;
   }
 
-// Inside the TvApp class in tv-app.js
+  constructor() {
+    super();
+    this.name = 'OwenDingTV';
+    this.source = './assets/channels.json';
+    this.listings = [];
+    this.activeItem = null;
+    this.currentIndex = 0; // Initialize to the first item
+  }
 
-render() {
-  const videoPlayerTemplate = this.activeItem && this.activeItem.metadata.source ? html`
-    <iframe
-      width="560"
-      height="315"
-      src="${this.activeItem.metadata.source}"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen>
-    </iframe>
-  ` : null; // If there's no video URL, render nothing.
+  render() {
+    return html`
+      <div class="lecture-container">
+        <div class="tv-channel-list">
+          ${this.listings.map((item, index) => html`
+            <tv-channel
+              title="${item.title}"
+              description="${item.description}"
+              .index="${index + 1}"
+              @click="${() => this.selectItem(index)}"
+            ></tv-channel>
+          `)}
+        </div>
+        <div class="lecture-screen">
+          ${this.activeItem && this.activeItem.metadata.source ? html`
+            <video-player
+              source="${this.activeItem.metadata.source}"
+              accent-color="orange"
+              dark
+            ></video-player>
+            <div id="navigationButtons">
+              <button ?disabled="${this.currentIndex <= 0}" @click="${this.prevItem}">Previous Page</button>
+              <button ?disabled="${this.currentIndex >= this.listings.length - 1}" @click="${this.nextItem}">Next Page</button>
+            </div>
+          ` : html`<p>Select a topic from the sidebar to show its content here.</p>`}
+        </div>
+      </div>
+    `;
+  }
 
-  // Render the sidebar with course titles and the main content with the video player and description.
-  return html`
-    <div class="sidebar">
-      <h2>${this.name}</h2>
-      ${this.courses.map(item => html`
-        <p @click="${() => this.selectItem(item)}">${item.title}</p>
-      `)}
-    </div>
-    <div class="main-content">
-      <h3>${this.activeItem ? this.activeItem.title : 'Select a Topic'}</h3>
-      <div id="videoPlayerContainer">${videoPlayerTemplate}</div>
-      <p>${this.activeItem ? this.activeItem.description : 'Select a topic from the sidebar to show its content here.'}</p>
-    </div>
-  `;
-}
-
-  
-  selectItem(item) {
-    this.activeItem = item;
-  
-    const videoContainer = this.shadowRoot.querySelector('#videoPlayerContainer');
-    const videoElement = videoContainer.querySelector('video') || document.createElement('video');
-    const sourceElement = videoElement.querySelector('source') || document.createElement('source');
-  
-    sourceElement.src = this.activeItem.videoUrl;
-    sourceElement.type = 'video/mp4';
-    
-    if (!videoElement.querySelector('source')) {
-      videoElement.appendChild(sourceElement);
-    }
-  
-    videoElement.load(); // This is necessary to load the new video after changing the source
-  
-    if (!videoContainer.querySelector('video')) {
-      videoContainer.appendChild(videoElement);
-    }
-  
+  selectItem(index) {
+    this.currentIndex = index;
+    this.activeItem = this.listings[index];
     this.requestUpdate();
   }
-  
-  
+
+  prevItem() {
+    if (this.currentIndex > 0) {
+      this.selectItem(this.currentIndex - 1);
+    }
+  }
+
+  nextItem() {
+    if (this.currentIndex < this.listings.length - 1) {
+      this.selectItem(this.currentIndex + 1);
+    }
+  }
 
   async connectedCallback() {
     super.connectedCallback();
-    await this.fetchCourses();
+    await this.fetchListings();
   }
 
-  async fetchCourses() {
+  async fetchListings() {
     try {
       const response = await fetch(this.source);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       const json = await response.json();
-      this.courses = json.data.items.map(item => {
-        // Assuming 'source' might be directly on the item, not inside 'metadata'
-        return {
-          ...item,
-          videoUrl: item.metadata?.source || item.source
-        };
-      });
-      this.requestUpdate(); // Ensure the component updates with the new data
+      if (json.status === 200 && json.data.items) {
+        this.listings = json.data.items;
+        this.activeItem = this.listings[this.currentIndex]; // Initialize the active item
+      }
     } catch (error) {
-      console.error('Could not fetch courses:', error);
+      console.error('Could not fetch listings:', error);
     }
   }
-}  
+}
 
 customElements.define('tv-app', TvApp);
